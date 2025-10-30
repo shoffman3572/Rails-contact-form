3299aa8722d5a9da4db7a6948ef389c8dea0c3f1
Stewart Hoffman <68552510+shoffman3572@users.noreply.github.com>
2025-10-30 06:55:49 -0700

    Add ContactsController with new and create actions

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,26 @@
+# app/controllers/contacts_controller.rb
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    # short-circuit bots that fill the hidden field
+    return redirect_to thanks_contacts_path if params.dig(:contact, :nickname).present?
+
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      # later you can email: ContactMailer.new_message(@contact).deliver_later
+      redirect_to thanks_contacts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def thanks; end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message, :nickname)
+  end
+end
