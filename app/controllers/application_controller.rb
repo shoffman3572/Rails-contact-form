3084f646c51390f61407fdc7d0ddcd77b86a4fc5
Stewart Hoffman <68552510+shoffman3572@users.noreply.github.com>
2025-10-30 08:44:01 -0700

    Remove X-Frame-Options header after response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  after_action do
+    response.headers.delete('X-Frame-Options')
+  end
 end
